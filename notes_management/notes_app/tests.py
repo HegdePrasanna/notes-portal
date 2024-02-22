bf9b891c1697c518fa3e8587e9a3c136b194a76a
Prasanna Hegde <prasann.h89@gmail.com>
2024-02-22 22:50:37 +0530

    adding test cases

--- notes_management/notes_app/tests.py
+++ notes_management/notes_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Notes
+class TestSetup(APITestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
 # Create your tests here.
--- notes_management/user_app/views.py
+++ notes_management/user_app/views.py
@@ -23,9 +23,9 @@ class RoleView(APIView):
         roles_queryset = Role.get_active()
         if roles_queryset.exists():
             serialized_data = serializers.RoleSerializer(roles_queryset, many=True)
-            return Response({'status': status.HTTP_200_OK, 'detail': 'Success', 'metadata':serialized_data.data})
+            return Response({'status': status.HTTP_200_OK, 'detail': 'Success', 'data':serialized_data.data})
         else:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Roles Not Found. Please Create a Role', 'metadata':[]},
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Roles Not Found. Please Create a Role', 'data':[]},
                             status=status.HTTP_404_NOT_FOUND)
         
         
@@ -60,9 +60,9 @@ class SingleRoleView(APIView):
         try:
             role_queryset = Role.objects.get(pk=pk, is_active=True, is_deleted=False)
         except Role.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested Role Not Found.', 'metadata':[]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested Role Not Found.', 'data':[]}, status=status.HTTP_404_NOT_FOUND)
         role_serializer = serializers.RoleSerializer(role_queryset)
-        return Response({'status': status.HTTP_200_OK, 'detail': 'Role successfully retrieved.', 'metadata':role_serializer.data})
+        return Response({'status': status.HTTP_200_OK, 'detail': 'Role successfully retrieved.', 'data':role_serializer.data})
     
     @swagger_auto_schema(
         request_body=serializers.RoleSerializer,
@@ -75,14 +75,14 @@ class SingleRoleView(APIView):
         try:
             role_queryset = Role.objects.get(pk=pk, is_active=True, is_deleted=False)
         except Role.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested Role Not Found.', 'metadata':[]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested Role Not Found.', 'data':[]}, status=status.HTTP_404_NOT_FOUND)
         if not request.user.is_superuser:
             return Response({'status': status.HTTP_401_UNAUTHORIZED, 'detail': "Only SuperUser Can Update Role", 'data':[]}, status=status.HTTP_401_UNAUTHORIZED)
         role_serializer = serializers.RoleSerializer(role_queryset, data=request.data, partial=True)
         if role_serializer.is_valid():
             role_serializer.save()
-            return Response({'status': status.HTTP_200_OK, 'detail': 'Role successfully updated.', 'metadata':role_serializer.data})
-        return Response({'status': status.HTTP_400_BAD_REQUEST, 'detail': 'Unable to update Role.', 'metadata':role_serializer.errors})
+            return Response({'status': status.HTTP_200_OK, 'detail': 'Role successfully updated.', 'data':role_serializer.data})
+        return Response({'status': status.HTTP_400_BAD_REQUEST, 'detail': 'Unable to update Role.', 'data':role_serializer.errors})
     
     @swagger_auto_schema(
         responses={200: serializers.RoleReturnSerializer(), 400:gs.Generic400Serializer(), 403:gs.Generic403Serializer(), 404:gs.Generic404Serializer(), 401:gs.Generic401Serializer()}
@@ -94,13 +94,13 @@ class SingleRoleView(APIView):
         try:
             role_queryset = Role.objects.get(pk=pk, is_active=True, is_deleted=False)
         except Role.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested Role Not Found.', 'metadata':[]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested Role Not Found.', 'data':[]}, status=status.HTTP_404_NOT_FOUND)
         if not request.user.is_superuser:
             return Response({'status': status.HTTP_401_UNAUTHORIZED, 'detail': "Only SuperUser Can Delete Role", 'data':[]}, status=status.HTTP_401_UNAUTHORIZED)
         role_queryset.is_deleted = True
         role_queryset.is_active = False
         role_queryset.save()
-        return Response({'status': status.HTTP_200_OK, 'detail': 'Role successfully Deleted.', 'metadata':[]})
+        return Response({'status': status.HTTP_200_OK, 'detail': 'Role successfully Deleted.', 'data':[]})
 
 
 class UserView(APIView):
@@ -117,9 +117,9 @@ class UserView(APIView):
         if user_queryset.exists():
             user_queryset = serializers.UserSerializer.setup_eager_loading(user_queryset)
             serialized_data = serializers.UserSerializer(user_queryset, many=True)
-            return Response({'status': status.HTTP_200_OK, 'detail': 'Success', 'metadata':serialized_data.data})
+            return Response({'status': status.HTTP_200_OK, 'detail': 'Success', 'data':serialized_data.data})
         else:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'User Not Found. Please Create a User', 'metadata':[]},
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'User Not Found. Please Create a User', 'data':[]},
                             status=status.HTTP_404_NOT_FOUND)
         
 
@@ -155,9 +155,9 @@ class SingleUserView(APIView):
         try:
             user_queryset = User.objects.get(pk=pk, is_active=True, is_deleted=False)
         except User.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested User Not Found.', 'metadata':[]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested User Not Found.', 'data':[]}, status=status.HTTP_404_NOT_FOUND)
         user_serializer = serializers.UserSerializer(user_queryset)
-        return Response({'status': status.HTTP_200_OK, 'detail': 'User Details successfully retrieved.', 'metadata':user_serializer.data})
+        return Response({'status': status.HTTP_200_OK, 'detail': 'User Details successfully retrieved.', 'data':user_serializer.data})
     
     @swagger_auto_schema(
         request_body=serializers.UserSerializerCreate,
@@ -170,15 +170,15 @@ class SingleUserView(APIView):
         try:
             user_queryset = User.objects.get(pk=pk, is_active=True, is_deleted=False)
         except User.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested User Not Found.', 'metadata':[]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested User Not Found.', 'data':[]}, status=status.HTTP_404_NOT_FOUND)
 
         if request.user.id != pk:
             return Response({'status': status.HTTP_401_UNAUTHORIZED, 'detail': "Not Authorized to Update Other User's Information", 'data':[]}, status=status.HTTP_401_UNAUTHORIZED)
         user_serializer = serializers.UserSerializerCreate(user_queryset, data=request.data, partial=True)
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response({'status': status.HTTP_200_OK, 'detail': 'User successfully updated.', 'metadata':user_serializer.data})
-        return Response({'status': status.HTTP_400_BAD_REQUEST, 'detail': 'Unable to update User.', 'metadata':user_serializer.errors})
+            return Response({'status': status.HTTP_200_OK, 'detail': 'User successfully updated.', 'data':user_serializer.data})
+        return Response({'status': status.HTTP_400_BAD_REQUEST, 'detail': 'Unable to update User.', 'data':user_serializer.errors})
     
     @swagger_auto_schema(
         responses={200: serializers.UserReturnSerializer(), 400:gs.Generic400Serializer(), 403:gs.Generic403Serializer(), 404:gs.Generic404Serializer(), 401:gs.Generic401Serializer()}
@@ -190,13 +190,13 @@ class SingleUserView(APIView):
         try:
             user_queryset = User.objects.get(pk=pk, is_active=True, is_deleted=False)
         except User.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested User Not Found.', 'metadata':[]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': status.HTTP_404_NOT_FOUND, 'detail': 'Requested User Not Found.', 'data':[]}, status=status.HTTP_404_NOT_FOUND)
         if request.user.id != pk:
             return Response({'status': status.HTTP_401_UNAUTHORIZED, 'detail': "Not Authorized to Delete Other User", 'data':[]}, status=status.HTTP_401_UNAUTHORIZED)
         user_queryset.is_deleted = True
         user_queryset.is_active = False
         user_queryset.save()
-        return Response({'status': status.HTTP_200_OK, 'detail': 'User successfully Deleted.', 'metadata':[]})
+        return Response({'status': status.HTTP_200_OK, 'detail': 'User successfully Deleted.', 'data':[]})
 
 
 class LoginView(APIView):
