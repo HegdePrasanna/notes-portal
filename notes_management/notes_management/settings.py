2321c5a6f36c7dc07bd5494b477ea0a662837dcb
Prasanna Hegde <prasann.h89@gmail.com>
2024-02-23 23:05:21 +0530

    Deployment Changes Enabled allow host to * but it is not recommended added static files

--- notes_management/notes_management/settings.py
+++ notes_management/notes_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$fxi_chhl96#p)tnay%kiw854^423kev=h)84wg%e%cdq5e-ra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
